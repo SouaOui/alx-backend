800e40f6a282ee65a3aaf8c8acb4661d15a4af98
SouaOui <your.email@example.com>
2024-05-28 12:03:37 +0100

    Fetch data pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
+""" module doc """
 import csv
-from typing import List
-from typing import Tuple
+import math
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """ func doc """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -23,15 +24,15 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-        """ method doc """
-        start = (page - 1) * page_size
-        end = page * page_size
-        return (start, end)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ method doc """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         start, end = self.index_range(page, page_size)
         return self.dataset()[start:end]
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """ method doc """
+        start = (page - 1) * page_size
+        end = page * page_size
+        return (start, end)
