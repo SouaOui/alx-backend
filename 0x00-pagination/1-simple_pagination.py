5bfaf6b709602ed2a0f620d7f039a5cd2b12e7cd
SouaOui <your.email@example.com>
2024-05-28 11:58:05 +0100

    Fetch data pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+import csv
+from typing import List
+from typing import Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """ method doc """
+        start = (page - 1) * page_size
+        end = page * page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ method doc """
+        assert isinstance(page, int)
+        assert page > 0
+        assert isinstance(page_size, int)
+        assert page_size > 0
+        dataset = self.dataset()
+        start, end = self.index_range(page, page_size)
+        return dataset[start:end]
