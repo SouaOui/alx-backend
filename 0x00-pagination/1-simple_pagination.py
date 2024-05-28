4f57cd88959aa9f4600e6cc4640dbe2e4edac282
SouaOui <your.email@example.com>
2024-05-28 12:00:48 +0100

    Fetch data pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,10 +31,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ method doc """
-        assert isinstance(page, int)
-        assert page > 0
-        assert isinstance(page_size, int)
-        assert page_size > 0
-        dataset = self.dataset()
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
         start, end = self.index_range(page, page_size)
-        return dataset[start:end]
+        return self.dataset()[start:end]
